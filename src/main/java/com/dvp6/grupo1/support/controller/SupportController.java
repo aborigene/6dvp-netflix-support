488eb46a66df6e1d1c386e9ca7021a9616ce6428
guirubio <guilherme.rubio@outlook.com.br>
2021-04-26 15:30:00 -0300

    Ajustes nas rotas

--- src/main/java/com/dvp6/grupo1/support/controller/SupportController.java
+++ src/main/java/com/dvp6/grupo1/support/controller/SupportController.java
@@ -10,11 +10,7 @@ import com.dvp6.grupo1.support.model.SupportEntity;
 import com.dvp6.grupo1.support.model.SupportRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,17 +26,6 @@ public class SupportController {
     */
     @Autowired
     private SupportRepository supportRepository;
-
-
-    /*
-      Criando a rota /openTicket com o verbo POST.
-      Responsável por abertura de um novo chamado.
-    */
-    @PostMapping("/openTicket")
-    public ResponseEntity<String> openTicket(@RequestBody SupportEntity supportEntity) {
-        supportRepository.save(supportEntity);
-        return new ResponseEntity<>("Ticket aberto com sucesso!", HttpStatus.OK);
-    }
 
     /*
       Criando a rota /getAllTickets com o verbo GET.
