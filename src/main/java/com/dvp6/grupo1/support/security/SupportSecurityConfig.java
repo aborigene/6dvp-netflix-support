12de2646b84103677cc7265f187bb90de941f2fa
guirubio <guilherme.rubio@outlook.com.br>
2021-04-28 14:30:12 -0300

    ajuste nas rotas

--- src/main/java/com/dvp6/grupo1/support/security/SupportSecurityConfig.java
+++ src/main/java/com/dvp6/grupo1/support/security/SupportSecurityConfig.java
@@ -7,7 +7,6 @@
 package com.dvp6.grupo1.support.security;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -17,14 +16,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 */
 @EnableWebSecurity
 public class SupportSecurityConfig extends WebSecurityConfigurerAdapter {
-
-  /*
-   * Método responsável por deixar a documentação anomina.
-   */
-  @Override
-  public void configure(WebSecurity http) throws Exception {
-    http.ignoring().antMatchers("/swagger-support");
-  }
 
   /*
    * Método responsável filtar as permissões para as rotas com base no token jwt.
